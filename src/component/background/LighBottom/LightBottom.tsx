2247ff432b6ed653369a4bb01b7958cd3167f02f
AlexeyNekrasov <necrasoff.alex@yandex.ru>
2023-03-31 15:56:29 +0000

    finish

--- src/component/background/LighBottom/LightBottom.tsx
+++ src/component/background/LighBottom/LightBottom.tsx
@@ -7,8 +7,8 @@ const LightBottom = () => {
   return (
     <div className='swapanimation h-full w-full relative flex justify-center z-50 '>
       <div className='swapbutton flex  z-50  absolute'>
-          <span className='s1'></span>
-          <span className='s2 mr-20'></span>
+          <span className='s1 z-10 '></span>
+          <span className='s2 '></span>
       </div>
     </div>
 
